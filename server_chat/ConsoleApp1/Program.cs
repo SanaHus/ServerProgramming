31c4ab8305f8d91e6eb12006aeffff2781f371af
Serleim <cyu0304@gmail.com>
2022-04-27 20:09:02 +0900

    양방향 통신 ㄱㄴ

---
+++ server_chat/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+public class ServerSocket1
+{
+    public static void Main()
+    {
+        try
+        {
+            bool status = true;
+            string serverid = "[주] ";
+            string clientid = "[수] ";
+            string servermessage = "";
+            string clientmessage = "";
+            TcpListener tcpListener = new TcpListener(9000);
+            tcpListener.Start();
+            Console.WriteLine("Wating Connection...");
+            Socket socketForClient = tcpListener.AcceptSocket();
+            Console.WriteLine("'수' 님이 127.0.0.1에서 접속하셨습니다.");
+            NetworkStream networkStream = new NetworkStream(socketForClient);
+            StreamWriter streamwriter = new StreamWriter(networkStream);
+            StreamReader streamreader = new StreamReader(networkStream);
+
+
+            while (status)
+            {
+                if (socketForClient.Connected)
+                {
+                    servermessage = streamreader.ReadLine();
+                    Console.WriteLine(clientid + ": " + servermessage);
+                    if (servermessage == "/q")
+                    {
+                        status = false;
+                        streamreader.Close();
+                        networkStream.Close();
+                        streamwriter.Close();
+                        return;
+                    }
+                    Console.Write(serverid + ": ");
+                    clientmessage = Console.ReadLine();
+                    streamwriter.WriteLine(clientmessage);
+                    streamwriter.Flush();
+                }
+            }
+            streamreader.Close();
+            networkStream.Close();
+            streamwriter.Close();
+            socketForClient.Close();
+            Console.WriteLine("나갔어");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
---
+++ server_chat/ConsoleApp3/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+public class ClientSocket1
+{
+    static void Main(string[] args)
+    {
+        TcpClient socketForServer;
+        bool status = true;
+        try
+        {
+            string ip = Console.ReadLine();
+            if(ip != "/c 127.0.0.1:9000")
+            {
+                Console.WriteLine("다시 입력해~");
+                ip = Console.ReadLine();
+            }
+
+            Console.WriteLine("127.0.0.1:9000에 접속시도중...");
+            Console.WriteLine("'주'님께 연결되었습니다. ");
+            socketForServer = new TcpClient("localhost", 9000);
+        }
+        catch
+        {
+            Console.WriteLine("Failed to Connect to server{0}:999", "localhost");
+            return;
+        }
+        NetworkStream networkStream = socketForServer.GetStream();
+        StreamReader streamreader = new StreamReader(networkStream);
+        StreamWriter streamwriter = new StreamWriter(networkStream);
+
+        try
+        {
+            string clientid = "[수] ";
+            string serverid = "[주] ";
+            string clientmessage = "";
+            string servermessage = "";
+            while (status)
+            {
+                if (serverid == null)
+                {
+                    serverid = streamreader.ReadLine();
+                }
+                Console.Write(clientid + ": ");
+                clientmessage = Console.ReadLine();
+                if ((clientmessage == "/q") || (clientmessage == "/Q"))
+                {
+                    status = false;
+                    streamwriter.WriteLine("채팅 끝");
+                    streamwriter.Flush();
+                    streamreader.Close();
+                    networkStream.Close();
+                    streamwriter.Close();
+                }
+                if ((clientmessage != "/q") && (clientmessage != "/Q"))
+                {
+                    streamwriter.WriteLine(clientmessage);
+                    streamwriter.Flush();
+                    servermessage = streamreader.ReadLine();
+                    Console.WriteLine(serverid + ": " + servermessage);
+                }
+            }
+        }
+        catch
+        {
+            Console.WriteLine("서버 꺼짐");
+        }
+        streamreader.Close();
+        networkStream.Close();
+        streamwriter.Close();
+    }
+}
